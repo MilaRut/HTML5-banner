aa580673fa06ad5a4e97b9e8195cf27952a3d4e0
Mila Rut <mila.balashova2312@gmail.com>
2022-11-26 21:40:09 +0300

    fix script

--- script.js
+++ script.js
@@ -1,26 +1,34 @@
-const slides = document.querySelectorAll('.slides__item');
+(function () {
 
-let index = 0;
+    const slides = document.querySelectorAll('.slides__item');
 
-const activeSlide = n => {
-    for(slide of slides) {
-        slide.classList.remove('active');
+    let index = 0;
+
+    const activeSlide = n => {
+        for (slide of slides) {
+            slide.classList.remove('active');
+        }
+        slides[n].classList.add('active');
+    };
+
+    const prepareCurrentSlide = ind => {
+        activeSlide(index);
+    };
+
+    const nextSlide = () => {
+        if (index == slides.length - 1) {
+            index = 0;
+            prepareCurrentSlide(index);
+        } else {
+            index++;
+            prepareCurrentSlide(index);
+        }
+    };
+
+    const runPlay = () => {
+        nextSlide();
+        setTimeout(runPlay, 3000);
     }
-    slides[n].classList.add('active');
-};
 
-const prepareCurrentSlide = ind => {
-    activeSlide(index);
-};
-
-const nextSlide = () => {
-    if(index == slides.length - 1) {
-        index = 0;
-        prepareCurrentSlide(index);
-    } else {
-    index++;
-    prepareCurrentSlide(index);
-    }
-};
-
-let autoPlay = setInterval(() => nextSlide(), 3000);
+    runPlay();
+})();
